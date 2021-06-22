fd29cf44b2cc3d8eefc4bcccfcc8d7912ebf2ab3
ccocquempot <c.cocquempot@niryo.com>
2021-06-22 16:42:33 +0200

    correction on CMakeLists not installing some executable at the correct place. Add installation of tcp_server for niryo_robot_user_interface

--- niryo_robot_user_interface/setup.py
+++ niryo_robot_user_interface/setup.py
@@ -2,8 +2,7 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['niryo_robot_user_interface'],
-    # scripts=['bin/myscript'],
+    packages=['niryo_robot_user_interface', 'niryo_robot_user_interface.tcp_server'],
     package_dir={'': 'src'}
 )
 
