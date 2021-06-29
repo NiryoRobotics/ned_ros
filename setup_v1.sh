a7a466132afaec73b5ba353ec88e2c092b4e9e65
ccocquempot <c.cocquempot@niryo.com>
2021-06-29 17:17:15 +0200

    small correction

--- setup_v1.sh
+++ setup_v1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script to set all the configuration files for NED V1 as default
 
-cp ./niryo_robot_tools_commander/config/end_effectors_v1.yaml ./niryo_robot_toolsi_commander/config/end_effectors.yaml
+cp ./niryo_robot_tools_commander/config/end_effectors_v1.yaml ./niryo_robot_tools_commander/config/end_effectors.yaml
 cp ./niryo_robot_hardware_stack/joints_interface/config/ned/dynamixels_params_v1.yaml ./niryo_robot_hardware_stack/joints_interface/config/ned/dynamixels_params.yaml
 cp ./niryo_robot_hardware_stack/joints_interface/config/joints_params_v1.yaml ./niryo_robot_hardware_stack/joints_interface/config/joints_params.yaml
 cp ./niryo_robot_hardware_stack/joints_interface/config/motors_param_v1.yaml ./niryo_robot_hardware_stack/joints_interface/config/motors_param.yaml
