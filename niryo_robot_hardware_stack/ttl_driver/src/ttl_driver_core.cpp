3a0c4c8c273896d42ecf4ca8ab656f330eac8c5a
ccocquempot <c.cocquempot@niryo.com>
2021-07-12 18:35:20 +0200

    set namespace to relative for ttl and can driver

--- niryo_robot_hardware_stack/ttl_driver/src/ttl_driver_core.cpp
+++ niryo_robot_hardware_stack/ttl_driver/src/ttl_driver_core.cpp
@@ -95,16 +95,16 @@ void TtlDriverCore::initParameters(ros::NodeHandle& nh)
     double read_data_frequency = 0.0;
     double read_status_frequency = 0.0;
 
-    _nh.getParam("/niryo_robot_hardware_interface/ttl_driver/dxl_hardware_control_loop_frequency",
+    nh.getParam("dxl_hardware_control_loop_frequency",
                  _control_loop_frequency);
 
-    _nh.getParam("/niryo_robot_hardware_interface/ttl_driver/dxl_hardware_write_frequency",
+    nh.getParam("dxl_hardware_write_frequency",
                  write_frequency);
 
-    _nh.getParam("/niryo_robot_hardware_interface/ttl_driver/dxl_hardware_read_data_frequency",
+    nh.getParam("dxl_hardware_read_data_frequency",
                  read_data_frequency);
 
-    _nh.getParam("/niryo_robot_hardware_interface/ttl_driver/dxl_hardware_read_status_frequency",
+    nh.getParam("dxl_hardware_read_status_frequency",
                  read_status_frequency);
 
     ROS_DEBUG("TtlDriverCore::initParameters - dxl_hardware_control_loop_frequency : %f", _control_loop_frequency);
