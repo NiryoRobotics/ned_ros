35a74ab93ca8ba507b4195f4621bfd387e532ec2
ccocquempot <c.cocquempot@niryo.com>
2021-07-13 18:10:13 +0200

    correction for unit tests ttl

--- niryo_robot_hardware_stack/ttl_driver/test/ttl_driver_unit_tests.cpp
+++ niryo_robot_hardware_stack/ttl_driver/test/ttl_driver_unit_tests.cpp
@@ -25,10 +25,11 @@
 // Declare a test
 TEST(TtlDriverTestSuite, testInitDriver)
 {
-    ttl_driver::TtlDriver ttl_driver;
-    ttl_driver.scanAndCheck();
+  ros::NodeHandle nh("~");
+  ttl_driver::TtlDriver ttl_driver(nh);
+  ttl_driver.scanAndCheck();
 
-    EXPECT_TRUE(ttl_driver.isConnectionOk());
+  EXPECT_TRUE(ttl_driver.isConnectionOk());
 }
 
 // Run all the tests that were declared with TEST()
@@ -38,14 +39,6 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "ttl_driver_unit_tests");
 
   ros::NodeHandle nh;
-
-  // set log level as Debug.
-  // For tests launched using catkin_make run_tests, the logs are located in files only
-  // see build/test_results/ttl_driver/rostest-test_ttl_driver_unit_tests.xml
-  // log file is of the form .ros/log/aac4b920-c76b-11eb-942e-00e04c680780/ttl_driver_unit_tests-1*.log
-
-  if ( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) )
-     ros::console::notifyLoggerLevelsChanged();
 
   return RUN_ALL_TESTS();
 }
