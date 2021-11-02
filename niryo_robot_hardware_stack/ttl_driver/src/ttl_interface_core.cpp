808bd5d49bf78f14808ab024a3664a17d44a9467
ccocquempot <c.cocquempot@niryo.com>
2021-11-02 18:15:32 +0100

    small adition

--- niryo_robot_hardware_stack/ttl_driver/src/ttl_interface_core.cpp
+++ niryo_robot_hardware_stack/ttl_driver/src/ttl_interface_core.cpp
@@ -690,7 +690,7 @@ int TtlInterfaceCore::addJoint(const std::shared_ptr<common::model::JointState>&
   int result = niryo_robot_msgs::CommandStatus::TTL_READ_ERROR;
 
   // save motor id to a list (this is used in syncread to get params at the same address for all motors)
-  _ttl_manager->addMotorList(jointState->getId());
+  _ttl_manager->addToMotorList(jointState->getId());
 
   // add dynamixel as a new tool
   _ttl_manager->addHardwareComponent(jointState);
@@ -789,7 +789,7 @@ int TtlInterfaceCore::setConveyor(const std::shared_ptr<common::model::ConveyorS
     lock_guard<mutex> lck(_control_loop_mutex);
 
     // add conveyor id for conveyor list of ttl manager
-    _ttl_manager->addConveyorList(state->getId());
+    _ttl_manager->addToConveyorList(state->getId());
 
     // add hw component before to get driver
     _ttl_manager->addHardwareComponent(state);
