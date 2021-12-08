a601160af032ae4587f6663ba935cc95ad6bb9ca
ccocquempot <c.cocquempot@niryo.com>
2021-12-08 18:18:37 +0100

    add params from Florian

--- niryo_robot_hardware_stack/joints_interface/src/joints_interface_core.cpp
+++ niryo_robot_hardware_stack/joints_interface/src/joints_interface_core.cpp
@@ -263,11 +263,10 @@ void JointsInterfaceCore::rosControlLoop()
                 _reset_controller = true;
             }
             bool isFreqMet = _control_loop_rate.sleep();
-            if (!isFreqMet)
-                ROS_DEBUG_THROTTLE(2,
-                           "JointsInterfaceCore::rosControlLoop : freq not met : expected (%f s) vs actual (%f s)",
-                           _control_loop_rate.expectedCycleTime().toSec(),
-                           _control_loop_rate.cycleTime().toSec());
+            ROS_WARN_COND(!isFreqMet,
+                          "JointsInterfaceCore::rosControlLoop : freq not met : expected (%f s) vs actual (%f s)",
+                          _control_loop_rate.expectedCycleTime().toSec(),
+                          _control_loop_rate.cycleTime().toSec());
         }
     }
 }
