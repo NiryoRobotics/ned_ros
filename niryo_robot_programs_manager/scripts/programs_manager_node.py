4b11754fe00bb70b76c649d898591b19118e387d
minhthuc <t.pham@niryo.com>
2021-12-10 11:14:52 +0100

    remove stuff

--- niryo_robot_programs_manager/scripts/programs_manager_node.py
+++ niryo_robot_programs_manager/scripts/programs_manager_node.py
@@ -183,16 +183,13 @@ class ProgramManagerNode:
         return self.__execute_program(language, name=name, code_string=code_string)
 
     def __callback_stop_program(self, _req):
-        rospy.INFO("test callback stop program called")
         if not self.__program_is_running.program_is_running:
             return CommandStatus.SUCCESS, "No program is running"
 
         try:
-            rospy.INFO("stop program button pressed")
             self.__loop_autorun = False
             ret, message = self.__python2_manager.stop_execution()
             if not self.__standalone:
-                rospy.INFO("have standalone")
                 self.__stop_robot_action()
 
             self.__program_is_running = ProgramIsRunning(False, ProgramIsRunning.PREEMPTED, "")
