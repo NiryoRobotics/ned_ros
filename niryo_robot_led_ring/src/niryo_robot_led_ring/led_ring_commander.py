d57405c8379bdb314b0531b7111c584f53e4ad12
Clément Cocquempot <c.cocquempot@niryo.com>
2021-12-20 14:07:02 +0000

    init ledring at the end of booting

--- niryo_robot_led_ring/src/niryo_robot_led_ring/led_ring_commander.py
+++ niryo_robot_led_ring/src/niryo_robot_led_ring/led_ring_commander.py
@@ -124,6 +124,7 @@ class LedRingCommander:
             if not self.__is_simulation:
                 from led_ring_utils import enable_led_ring
                 enable_led_ring(rospy.get_param('~enable_led_ring_bcm_pin'))
+                self.led_ring_anim.init_led_ring()
         #    self.led_ring_anim.fade(BLUE)
 
         if (self.robot_status != msg.robot_status or
