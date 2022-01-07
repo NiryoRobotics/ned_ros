4d9d2eb3dc79ba8bf09afc5840e671aab1150f12
Justin <j.mottier@niryo.com>
2022-01-07 13:56:19 +0100

    tuptimewrapper new way to use duration

--- niryo_robot_reports/src/niryo_robot_reports/metrics/TuptimeWrapper.py
+++ niryo_robot_reports/src/niryo_robot_reports/metrics/TuptimeWrapper.py
@@ -1,4 +1,3 @@
-import re
 import csv
 import subprocess
 
@@ -8,7 +7,7 @@ from GenericWrapper import GenericWrapper
 class TuptimeWrapper(GenericWrapper):
     def _fetch_datas(self):
         # root access is sometimes needed at boot
-        output = subprocess.check_output(['sudo', 'tuptime', '-c'])
+        output = subprocess.check_output(['sudo', 'tuptime', '-cs'])
         loutput = output.splitlines()
         reader = csv.reader(loutput)
         for row in reader:
@@ -16,25 +15,10 @@ class TuptimeWrapper(GenericWrapper):
             if key == 'system_startups':
                 self._data[key] = row[0]
             elif key in ['average_downtime', 'average_uptime', 'system_life']:
-                self._data[key] = self.__convert_duration(row[0])
+                self._data[key] = int(row[0])
             elif key == 'system_shutdowns':
-                self._data[key] = int(row[0]) + int(row[2])
+                self._data[key] = int(row[0]) + int(row[3])
             elif key in ['system_downtime', 'system_uptime']:
-                self._data[key] = self.__convert_duration(row[1])
+                self._data[key] = int(row[1])
             elif key == 'current_uptime':
-                self._data[key] = self.__convert_duration(row[0])
-
-    @staticmethod
-    def __convert_duration(d):
-        duration_regex = r'((?P<Y>[0-9]+)yr )?((?P<D>[0-9]+)d )?((?P<H>[0-9]+)h )?((?P<M>[0-9]+)m )?((?P<S>[0-9]+)s)'
-        match = re.match(duration_regex, d)
-        iso_format = 'P'
-        for i in 'YD':
-            if match.group(i):
-                iso_format += match.group(i) + i
-        iso_format += 'T'
-        for i in 'HMS':
-            if match.group(i):
-                iso_format += match.group(i) + i
-
-        return iso_format
+                self._data[key] = int(row[0])
