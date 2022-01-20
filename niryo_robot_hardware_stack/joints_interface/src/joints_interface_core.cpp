872b51befb48f32df5e8d78b96fe3a0e47c6921f
minhthuc <t.pham@niryo.com>
2022-01-20 10:22:28 +0100

    small change for make sure lock write before update ros control

--- niryo_robot_hardware_stack/joints_interface/src/joints_interface_core.cpp
+++ niryo_robot_hardware_stack/joints_interface/src/joints_interface_core.cpp
@@ -295,8 +295,8 @@ void JointsInterfaceCore::resetController()
     if (_hardware_version == "ned2")
     {
         _robot->write(ros::Time::now(), ros::Duration(0.0));
-        _cm->update(ros::Time::now(), ros::Duration(0.0), true);
         _lock_write_cnt = 150;
+        _cm->update(ros::Time::now(), ros::Duration(0.0), true);
     }
     else if (_hardware_version == "ned" || _hardware_version == "one")
     {
