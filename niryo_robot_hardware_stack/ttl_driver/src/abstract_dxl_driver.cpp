a5f89420d67a08efdce83e34702fe1e7cab2d4e8
Isaac Ambit Brao <i.ambit@niryo.com>
2022-10-28 13:27:49 +0200

    initial temperature limit & shutdown configuration for dxl xl330

--- niryo_robot_hardware_stack/ttl_driver/src/abstract_dxl_driver.cpp
+++ niryo_robot_hardware_stack/ttl_driver/src/abstract_dxl_driver.cpp
@@ -30,120 +30,124 @@ using ::common::model::EDxlCommandType;
 namespace ttl_driver
 {
 
-/**
- * @brief AbstractDxlDriver::AbstractDxlDriver
-*/
-AbstractDxlDriver::AbstractDxlDriver(std::shared_ptr<dynamixel::PortHandler> portHandler,
-                                     std::shared_ptr<dynamixel::PacketHandler> packetHandler) :
-    AbstractMotorDriver(std::move(portHandler), std::move(packetHandler))
-{}
-
-std::string AbstractDxlDriver::str() const
-{
-    return "Dynamixel Driver (" + AbstractMotorDriver::str() + ")";
-}
-
-/**
- * @brief AbstractDxlDriver::writeSingleCmd
- * @param cmd
-*/
-int AbstractDxlDriver::writeSingleCmd(const std::unique_ptr<common::model::AbstractTtlSingleMotorCmd >& cmd)
-{
-    if (cmd && cmd->isValid() && cmd->isDxlCmd())
+    /**
+     * @brief AbstractDxlDriver::AbstractDxlDriver
+     */
+    AbstractDxlDriver::AbstractDxlDriver(std::shared_ptr<dynamixel::PortHandler> portHandler,
+                                         std::shared_ptr<dynamixel::PacketHandler> packetHandler) : AbstractMotorDriver(std::move(portHandler), std::move(packetHandler))
     {
-        switch (EDxlCommandType(cmd->getCmdType()))
+    }
+
+    std::string AbstractDxlDriver::str() const
+    {
+        return "Dynamixel Driver (" + AbstractMotorDriver::str() + ")";
+    }
+
+    /**
+     * @brief AbstractDxlDriver::writeSingleCmd
+     * @param cmd
+     */
+    int AbstractDxlDriver::writeSingleCmd(const std::unique_ptr<common::model::AbstractTtlSingleMotorCmd> &cmd)
+    {
+        if (cmd && cmd->isValid() && cmd->isDxlCmd())
         {
-        case EDxlCommandType::CMD_TYPE_VELOCITY:
-            return writeVelocityGoal(cmd->getId(), cmd->getParam());
+            switch (EDxlCommandType(cmd->getCmdType()))
+            {
+            case EDxlCommandType::CMD_TYPE_VELOCITY:
+                return writeVelocityGoal(cmd->getId(), cmd->getParam());
+            case EDxlCommandType::CMD_TYPE_POSITION:
+                return writePositionGoal(cmd->getId(), cmd->getParam());
+            case EDxlCommandType::CMD_TYPE_EFFORT:
+                return writeTorqueGoal(cmd->getId(), static_cast<uint16_t>(cmd->getParam()));
+            case EDxlCommandType::CMD_TYPE_TORQUE:
+                return writeTorqueEnable(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
+            case EDxlCommandType::CMD_TYPE_LEARNING_MODE:
+                return writeTorqueEnable(cmd->getId(), static_cast<uint8_t>(!cmd->getParam()));
+            case EDxlCommandType::CMD_TYPE_PING:
+                return ping(cmd->getId());
+            case EDxlCommandType::CMD_TYPE_PID:
+            {
+                std::vector<uint16_t> params_conv;
+                for (auto p : cmd->getParams())
+                {
+                    params_conv.emplace_back(static_cast<uint16_t>(p));
+                }
+                return writePID(cmd->getId(), params_conv);
+            }
+            case EDxlCommandType::CMD_TYPE_PROFILE:
+                return writeVelocityProfile(cmd->getId(), cmd->getParams());
+            case EDxlCommandType::CMD_TYPE_CONTROL_MODE:
+                return writeControlMode(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
+            case EDxlCommandType::CMD_TYPE_LED_STATE:
+                return writeLed(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
+            case EDxlCommandType::CMD_TYPE_STARTUP:
+                return writeStartupConfiguration(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
+            case EDxlCommandType::CMD_TYPE_TEMPERATURE_LIMIT:
+                return writeTemperatureLimit(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
+            case EDxlCommandType::CMD_TYPE_SHUTDOWN:
+                return writeShutdownConfiguration(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
+            default:
+                std::cout << "Command not implemented " << cmd->getCmdType() << std::endl;
+            }
+        }
+
+        std::cout << "AbstractDxlDriver::writeSingleCmd : Command not validated: " << cmd->str() << std::endl;
+        return COMM_RX_CORRUPT;
+    }
+
+    /**
+     * @brief AbstractDxlDriver::writeSyncCmd
+     * @param type
+     * @param ids
+     * @param params
+     */
+    int AbstractDxlDriver::writeSyncCmd(int type, const std::vector<uint8_t> &ids, const std::vector<uint32_t> &params)
+    {
+        assert(!ids.empty() && "AbstractDxlDriver::writeSyncCmd: ids is empty");
+        assert(!params.empty() && "AbstractDxlDriver::writeSyncCmd: params is empty");
+
+        switch (EDxlCommandType(type))
+        {
         case EDxlCommandType::CMD_TYPE_POSITION:
-            return writePositionGoal(cmd->getId(), cmd->getParam());
+            return syncWritePositionGoal(ids, params);
+        case EDxlCommandType::CMD_TYPE_VELOCITY:
+            return syncWriteVelocityGoal(ids, params);
         case EDxlCommandType::CMD_TYPE_EFFORT:
-            return writeTorqueGoal(cmd->getId(), static_cast<uint16_t>(cmd->getParam()));
-        case EDxlCommandType::CMD_TYPE_TORQUE:
-            return writeTorqueEnable(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
-        case EDxlCommandType::CMD_TYPE_LEARNING_MODE:
-            return writeTorqueEnable(cmd->getId(), static_cast<uint8_t>(!cmd->getParam()));
-        case EDxlCommandType::CMD_TYPE_PING:
-            return ping(cmd->getId());
-        case EDxlCommandType::CMD_TYPE_PID:
         {
             std::vector<uint16_t> params_conv;
-            for (auto p : cmd->getParams())
+            params_conv.reserve(params.size());
+            for (auto const &p : params)
             {
                 params_conv.emplace_back(static_cast<uint16_t>(p));
             }
-            return writePID(cmd->getId(), params_conv);
+            return syncWriteTorqueGoal(ids, params_conv);
         }
-        case EDxlCommandType::CMD_TYPE_PROFILE:
-            return writeVelocityProfile(cmd->getId(), cmd->getParams());
-        case EDxlCommandType::CMD_TYPE_CONTROL_MODE:
-            return writeControlMode(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
-        case EDxlCommandType::CMD_TYPE_LED_STATE:
-            return writeLed(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
-        case EDxlCommandType::CMD_TYPE_STARTUP:
-            return writeStartupConfiguration(cmd->getId(), static_cast<uint8_t>(cmd->getParam()));
+        case EDxlCommandType::CMD_TYPE_TORQUE:
+        {
+            std::vector<uint8_t> params_conv;
+            params_conv.reserve(params.size());
+            for (auto const &p : params)
+            {
+                params_conv.emplace_back(static_cast<uint8_t>(p));
+            }
+            return syncWriteTorqueEnable(ids, params_conv);
+        }
+        case EDxlCommandType::CMD_TYPE_LEARNING_MODE:
+        {
+            std::vector<uint8_t> params_inv;
+            params_inv.reserve(params.size());
+            for (auto const &p : params)
+            {
+                params_inv.emplace_back(!p);
+            }
+            return syncWriteTorqueEnable(ids, params_inv);
+        }
         default:
-            std::cout << "Command not implemented " << cmd->getCmdType() << std::endl;
+            std::cout << "Command not implemented " << type << std::endl;
         }
+
+        std::cout << "AbstractDxlDriver::writeSyncCmd : Command not validated: " << type << std::endl;
+        return -1;
     }
 
-    std::cout << "AbstractDxlDriver::writeSingleCmd : Command not validated: " << cmd->str() << std::endl;
-    return COMM_RX_CORRUPT;
-}
-
-/**
- * @brief AbstractDxlDriver::writeSyncCmd
- * @param type
- * @param ids
- * @param params
-*/
-int AbstractDxlDriver::writeSyncCmd(int type, const std::vector<uint8_t>& ids, const std::vector<uint32_t>& params)
-{
-    assert(!ids.empty() && "AbstractDxlDriver::writeSyncCmd: ids is empty");
-    assert(!params.empty() && "AbstractDxlDriver::writeSyncCmd: params is empty");
-
-    switch (EDxlCommandType(type))
-    {
-    case EDxlCommandType::CMD_TYPE_POSITION:
-        return syncWritePositionGoal(ids, params);
-    case EDxlCommandType::CMD_TYPE_VELOCITY:
-        return syncWriteVelocityGoal(ids, params);
-    case EDxlCommandType::CMD_TYPE_EFFORT:
-    {
-        std::vector<uint16_t> params_conv;
-        params_conv.reserve(params.size());
-        for (auto const& p : params)
-        {
-            params_conv.emplace_back(static_cast<uint16_t>(p));
-        }
-        return syncWriteTorqueGoal(ids, params_conv);
-    }
-    case EDxlCommandType::CMD_TYPE_TORQUE:
-    {
-        std::vector<uint8_t> params_conv;
-        params_conv.reserve(params.size());
-        for (auto const& p : params)
-        {
-            params_conv.emplace_back(static_cast<uint8_t>(p));
-        }
-        return syncWriteTorqueEnable(ids, params_conv);
-    }
-    case EDxlCommandType::CMD_TYPE_LEARNING_MODE:
-    {
-        std::vector<uint8_t> params_inv;
-        params_inv.reserve(params.size());
-        for (auto const& p : params)
-        {
-            params_inv.emplace_back(!p);
-        }
-        return syncWriteTorqueEnable(ids, params_inv);
-    }
-    default:
-        std::cout << "Command not implemented " << type << std::endl;
-    }
-
-    std::cout << "AbstractDxlDriver::writeSyncCmd : Command not validated: " << type << std::endl;
-    return -1;
-}
-
-}  // namespace ttl_driver
+} // namespace ttl_driver
