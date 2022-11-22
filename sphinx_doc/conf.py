33c84f512a344d0ffff4ea9d70d51caeecd4c1f5
Justin <j.mottier@niryo.com>
2022-11-22 12:23:29 +0100

    change versions

--- sphinx_doc/conf.py
+++ sphinx_doc/conf.py
@@ -20,9 +20,9 @@ copyright = shared_conf.copyright
 author = shared_conf.author
 
 # The short X.Y version
-version = u'v4.0'
+version = u'v4.1'
 # The full version, including alpha/beta/rc tags
-release = u'v4.1.0'
+release = u'v4.1.1'
 
 # -- General configuration ---------------------------------------------------
 
