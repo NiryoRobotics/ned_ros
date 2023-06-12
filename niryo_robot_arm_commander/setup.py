8aa900ca0da3d60b95da1c7f3ce4646c950c1c74
Justin <j.mottier@niryo.com>
2023-06-12 17:09:46 +0200

    resolve build error

--- niryo_robot_arm_commander/setup.py
+++ niryo_robot_arm_commander/setup.py
@@ -1,11 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    # packages=['niryo_robot_arm_commander'],
-    packages=find_packages(exclude=['test']),
+    packages=['niryo_robot_arm_commander'],
     # scripts=['bin/myscript'],
-    package_dir={'': 'src'}
-)
+    package_dir={'': 'src'})
 
 setup(**d)
