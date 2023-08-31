f44f6a752fbd5d975b9aa503ffa1b5c563c84401
Michaël Gross <m.gross@niryo.com>
2023-08-31 13:34:27 +0200

    fix wrong orientation of gripper

--- niryo_robot_vision/scripts/ObjectDetector.py
+++ niryo_robot_vision/scripts/ObjectDetector.py
@@ -194,7 +194,7 @@ class ObjectDetector:
 
                 # ANY CASE
                 if self._obj_type == ObjectType.ANY:
-                    if len(approx) > 5:
+                    if len(approx) >= 5:
                         obj_type = ObjectType.CIRCLE
                     else:
                         obj_type = ObjectType.SQUARE
@@ -205,7 +205,7 @@ class ObjectDetector:
                 else:
                     obj_type = self._obj_type
                     # Circle Case
-                    if self._obj_type == ObjectType.CIRCLE and len(approx) > 5:
+                    if self._obj_type == ObjectType.CIRCLE and len(approx) >= 5:
                         found_something = True
                         break
 
