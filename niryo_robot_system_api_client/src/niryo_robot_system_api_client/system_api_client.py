dec445245bf988b551127b0be44773893d3e8b52
Justin <j.mottier@niryo.com>
2023-09-07 11:42:51 +0200

    use requests.request instead of get and post

--- niryo_robot_system_api_client/src/niryo_robot_system_api_client/system_api_client.py
+++ niryo_robot_system_api_client/src/niryo_robot_system_api_client/system_api_client.py
@@ -5,80 +5,74 @@ from .SystemApiResponse import SystemApiResponse
 from .SystemApiEndpoint import SystemApiEndpoint
 
 
-def __request(requests_method, endpoint, *args, **kwargs):
+def __request(method, endpoint, params=None, body_params=None):
     try:
-        requests_response = requests_method(f'{API_BASE_URL}{endpoint.value}', *args, **kwargs)
+        requests_response = requests.request(method, f'{API_BASE_URL}{endpoint.value}', params=params, json=body_params)
     except requests.ConnectionError as connection_error:
-        raise ConnectionError(str(connection_error))
+        return SystemApiResponse(-1, None, str(connection_error), None)
 
     return SystemApiResponse.from_requests(requests_response)
 
 
-def __get(endpoint, params=None):
-    return __request(requests.get, endpoint, params=params)
-
-
-def __post(endpoint, params=None):
-    return __request(requests.post, endpoint, json=params)
-
-
 def set_robot_name(name):
-    return __post(SystemApiEndpoint.SET_ROBOT_NAME, {'name': name})
+    return __request('POST', SystemApiEndpoint.SET_ROBOT_NAME, body_params={'name': name})
 
 
 def hotspot_state():
-    return __get(SystemApiEndpoint.HOTSPOT_STATE)
+    return __request('GET', SystemApiEndpoint.HOTSPOT_STATE)
 
 
 def wifi_state():
-    return __get(SystemApiEndpoint.WIFI_STATE)
+    return __request('GET', SystemApiEndpoint.WIFI_STATE)
 
 
 def reset_wifi():
-    return __post(SystemApiEndpoint.RESET_WIFI)
+    return __request('POST', SystemApiEndpoint.RESET_WIFI)
 
 
 def reset_hotspot():
-    return __post(SystemApiEndpoint.RESET_HOTSPOT)
+    return __request('POST', SystemApiEndpoint.RESET_HOTSPOT)
 
 
 def reset_ethernet():
-    return __post(SystemApiEndpoint.RESET_ETHERNET)
+    return __request('POST', SystemApiEndpoint.RESET_ETHERNET)
 
 
 def start_hotspot():
-    return __post(SystemApiEndpoint.START_HOTSPOT)
+    return __request('POST', SystemApiEndpoint.START_HOTSPOT)
 
 
 def restart_wifi():
-    return __post(SystemApiEndpoint.RESTART_WIFI)
+    return __request('POST', SystemApiEndpoint.RESTART_WIFI)
 
 
 def start_wifi():
-    return __post(SystemApiEndpoint.START_WIFI)
+    return __request('POST', SystemApiEndpoint.START_WIFI)
 
 
 def stop_wifi():
-    return __post(SystemApiEndpoint.STOP_WIFI)
+    return __request('POST', SystemApiEndpoint.STOP_WIFI)
 
 
 def stop_hotspot():
-    return __post(SystemApiEndpoint.STOP_HOTSPOT)
+    return __request('POST', SystemApiEndpoint.STOP_HOTSPOT)
 
 
 def ethernet_profile(profile, ip="", mask="", gateway="", dns=[]):
-    return __post(SystemApiEndpoint.ETHERNET_PROFILE, {
-        "profile": profile,
-        "ip": ip,
-        "mask": mask,
-        "gw": gateway,
-        "dns": dns,
-    })
+    return __request('POST',
+                     SystemApiEndpoint.ETHERNET_PROFILE,
+                     body_params={
+                         "profile": profile,
+                         "ip": ip,
+                         "mask": mask,
+                         "gw": gateway,
+                         "dns": dns,
+                     })
 
 
 def set_setting(name, value):
-    return __post(SystemApiEndpoint.SET_SETTING, {'name': name, 'value': value})
+    return __request('POST', SystemApiEndpoint.SET_SETTING, body_params={'name': name, 'value': value})
 
 
 def get_setting(name):
-    return __get(SystemApiEndpoint.GET_SETTING, {'name': name})
+    return __request('GET', SystemApiEndpoint.GET_SETTING, {'name': name})
