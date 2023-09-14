23e526b405a6201065a2fca8157e4b90a69c2c26
Justin <j.mottier@niryo.com>
2023-09-14 10:51:46 +0200

    add metrics to daily reports

--- niryo_robot_reports/src/niryo_robot_reports/DailyReport.py
+++ niryo_robot_reports/src/niryo_robot_reports/DailyReport.py
@@ -2,7 +2,7 @@ from niryo_robot_reports.Report import Report
 
 
 class DailyReport(Report):
-    DEFAULT_VALUE = {'logs': []}
+    DEFAULT_VALUE = {'logs': [], 'metrics': {}}
 
     def add_log(self, msg, log_type, date):
         inserted = False
@@ -12,11 +12,13 @@ class DailyReport(Report):
                 inserted = True
                 break
         if not inserted:
-            self.content['logs'].append({
-                'msg': msg,
-                'type': log_type,
-                'occurred': [date]
-            })
+            self.content['logs'].append({'msg': msg, 'type': log_type, 'occurred': [date]})
+        self._write()
+
+    def add_metrics(self, metric_name, metric_value):
+        if metric_name in self.content['metrics'] and isinstance(self.content['metrics'][metric_name], list):
+            self.content['metrics'][metric_name].append(metric_value)
+        self.content['metrics'][metric_name] = metric_value
         self._write()
 
     def set_date(self, date):
