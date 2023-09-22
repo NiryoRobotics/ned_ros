b49bb1ed7bcddab537d5d964f050d3c0d7cfbff4
Justin <j.mottier@niryo.com>
2023-09-22 18:09:57 +0200

    WIP mapping

--- niryo_robot_modbus/src/niryo_robot_modbus/InputRegisterDataBlock.py
+++ niryo_robot_modbus/src/niryo_robot_modbus/InputRegisterDataBlock.py
@@ -2,14 +2,18 @@
 
 from pymodbus.datastore import ModbusSparseDataBlock
 
+from .mapping import input_register_mapping
+
 
 class InputRegisterDataBlock(ModbusSparseDataBlock):
     """
     Input Register: Read Only 16-bit data
     """
 
-    def __init__(self):
-        super().__init__([])
+    def __init__(self, ros_wrapper):
+        self.__ros_wrapper = ros_wrapper
+        self.__mapping = input_register_mapping.get_mapping(self.__ros_wrapper)
+        super().__init__(self.__mapping.keys())
 
     def getValues(self, address, count=1):
         super().getValues(address, count)
