b37e6de92bce31e741cc5c2ff90d6b3eb3b83285
Justin Mottier <j.mottier@niryo.com>
2024-01-09 10:48:41 +0100

    package changes

--- niryo_robot_modbus/setup.py
+++ niryo_robot_modbus/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=['niryo_robot_modbus', 'niryo_robot_modbus.old'], package_dir={'': 'src'})
+d = generate_distutils_setup(packages=['niryo_robot_modbus'], package_dir={'': 'src'})
 
 setup(**d)
