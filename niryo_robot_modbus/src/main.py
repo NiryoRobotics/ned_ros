39ba691dc047cd63fbb61c8f0b3bae4812292e31
Justin Mottier <j.mottier@niryo.com>
2024-01-26 16:56:34 +0100

    rm dev file

--- niryo_robot_modbus/src/main.py
+++
@@ -1,7 +0,0 @@
-from niryo_robot_modbus.ModbusServer import ModbusServer
-if __name__ == "__main__":
-    server = ModbusServer('0.0.0.0', 5020)
-    try:
-        server.start()
-    except KeyboardInterrupt:
-        server.stop()
